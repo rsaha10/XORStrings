ed63389c6d9d648d6898ed8d6e3d635dbc86235f
rsaha10 <rsaha10@stuy.edu>
2020-11-02 06:47:41 -0500

    python starter script

---
+++ code.py
@@ -0,0 +1,13 @@
+import sys
+
+mode = sys.argv[1]
+keyfile = sys.argv[2]
+inpfile = sys.argv[3]
+key = open(keyfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+inp = open(inpfile).read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
+debug = False
+
+if(debug):
+    print("mode:"+mode)
+    print("key: "+key)
+    print("inp: "+inp)
